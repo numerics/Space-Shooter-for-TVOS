134f92dd5a79cc245fb5e5e9acf8db081c603c06
John Basile <jbasile54@gmail.com>
2016-01-31 22:27:49 -0800

    Initial Commit

---
+++ Space ShooterTests/Space_ShooterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Space_ShooterTests.swift
+//  Space ShooterTests
+//
+//  Created by John Basile on 1/31/16.
+//  Copyright © 2016 John Basile. All rights reserved.
+//
+
+import XCTest
+@testable import Space_Shooter
+
+class Space_ShooterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
